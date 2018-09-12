f3894fc76452f366ff29e8b601e6eba66d9200e6
Marcos Slomp <slomp@adobe.com>
2018-09-12 16:31:31 -0700

    fixing source amalgamations

--- src/amalg/amalg.cpp
+++ src/amalg/amalg.cpp
@@ -2,12 +2,10 @@
 #include "../treedump.cpp"
 #include "../system.cpp"
 #include "../debug-api.cpp"
-
-//#include "../../data/force_working_directory.cpp"
+#include "../halide-image-io.cpp"
 
 #include "../../third-party/imgui/imgui.cpp"
 #include "../../third-party/imgui/imgui_draw.cpp"
-//#include "../../third-party/imgui/imgui_demo.cpp"
 #include "../../third-party/imgui/examples/imgui_impl_glfw.cpp"
 #include "../../third-party/imgui/examples/imgui_impl_opengl2.cpp"
 
